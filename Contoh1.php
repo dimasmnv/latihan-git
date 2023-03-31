0732d3b8ed906fb6aeafb6adfc69e4bf1d848232
dimasmnv <128135472+dimasmnv@users.noreply.github.com>
2023-03-31 01:37:56 -0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Dimas Bagaskara
+ Saya tinggal di daerah Jakarta Barat
+ olah raga yang saya sukai adalah
+ Sepakbola";
+ }
+}
